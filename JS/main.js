f62a757a92285299b005f19295ae23823b6a057e
Jose Gerez <Jose.Gerez.DV@gmail.com>
2022-03-29 21:34:58 -0300

    se agrega el proyecto a GitHub

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+
+
+{
+    let artistas = `Artistas favoritos: \n`;
+
+
+
+for (let i = 0; i < 4; i++) {
+    artistas += `${prompt("ingrese nombre de su artista favorito")}\n`;
+}
+
+
+
+alert(artistas);
+
+}
