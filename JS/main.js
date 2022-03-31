0d98617fca00530f147d69c77632e1e33a11f67d
Jose Gerez <Jose.Gerez.DV@gmail.com>
2022-03-31 19:57:55 -0300

    se completa estructura del index

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,3 @@
 
 
-{
-    let artistas = `Artistas favoritos: \n`;
-
-
-
-for (let i = 0; i < 4; i++) {
-    artistas += `${prompt("ingrese nombre de su artista favorito")}\n`;
-}
-
-
-
-alert(artistas);
-
-}
 
