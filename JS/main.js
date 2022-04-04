4a8a3793430c429766bcb102e824b93a9407d2a3
Jose Gerez <Jose.Gerez.DV@gmail.com>
2022-04-04 14:27:24 -0300

    ejercicio de preguntas al usuario con respuestas e IVA

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,44 @@
 
+let valor = 0;
+
+let precio2 = 0;
+
+
+function valorProducto(){
+
+    let producto = prompt("que producto desea agregar? tv - celular - heladera");    
+
+    if(producto == "tv") {
+        valor += 30000
+    }
+    if(producto == "celular") {
+        valor += 50000
+    }
+    if(producto == "heladera") {
+        valor += 45000
+    }
+
+}
+
+function sumaProductos(valor,precio2){
+    let suma;
+    suma = (valor) * 1.21;
+    alert("sus productos tienen un valor final de " + suma + " mas IVA");
+}
+
+//llamamos funcion
+valorProducto ();
+
+alert("Su producto tiene un falor final de " + valor);
+
+let otroProducto = confirm("desea agregar otro producto (Aceptar -Si- cancelar -NO-?)")
+
+if (otroProducto == true){
+   valorProducto();
+}
+
+//llamamos funcion de suma
+sumaProductos(valor,precio2)
+
 
 
